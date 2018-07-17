b1fd97d5861dd8c18a8c8a09865c76eb90e79937
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2018-07-17 12:00:01 +0200

    update candidate form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('redirect/', views.redirect_login, name='redirect_login'),
     path('profile/<int:pk>/', views.user_detail, name='redirect_login'),
--- election/tests.py
+++ election/tests.py
@@ -5,11 +5,17 @@ import time
 from django.test import TestCase
 from django.conf import settings
 from django.core import mail
+from django.core.exceptions import FieldError
 from django.urls.base import reverse_lazy
+from django.test.client import Client
+from django.contrib import auth
 
 from selenium.webdriver.common.keys import Keys
 
 from utils.test import FunctionalTest
+from vote.models import User, Election, Candidate, Voter, Supervisor
+from election import views
+
 
 
 class ElectionCreationTest(FunctionalTest):
@@ -34,14 +40,14 @@ class ElectionCreationTest(FunctionalTest):
 
         # She adds some candidates
         for i in range(5):
-            inputbox = self.browser.find_element_by_id('id_first_name')
+            inputbox = self.browser.find_element_by_id('id_label')
             self.wait_for(inputbox.send_keys, 'Fakecandidate')
-            inputbox = self.browser.find_element_by_id('id_last_name')
-            self.wait_for(inputbox.send_keys, 'Number' + str(i))
-            inputbox = self.browser.find_element_by_id('id_program')
+            inputbox = self.browser.find_element_by_id('id_description')
             self.wait_for(inputbox.send_keys, 'Fake program')
             btn = self.wait_for(self.browser.find_element_by_id, 'form-btn')
             btn.click()
+            inputvalue = inputbox.get_attribute('value')
+            self.assertEqual(inputvalue, "")
             text = self.wait_for(self.browser.find_elements_by_xpath, "//*[contains(text(), 'Number"+str(i)+"')]")
 
         # She adds some voters
@@ -68,3 +74,69 @@ class ElectionCreationTest(FunctionalTest):
         # Finally she starts the election
         start = self.wait_for(self.browser.find_element_by_id, 'go_start')
         start.click()
+
+
+class ElectionDeleteTest(TestCase):
+
+    def setUp(self):
+        # create the client
+        self.client = Client()
+        self.user = User.objects.create_user("fred", "fred@test.fr", "secret")
+        self.client.login(username="fred", password="secret")
+        self.assertEqual(self.user, auth.get_user(self.client))
+
+        # create the election with its candidates and voters
+        self.election = Election.objects.create(name="lion", note="roar")
+        self.supervisor = Supervisor.objects.create(election=self.election,
+                                                    user=self.user)
+        self.users = []
+        self.candidates = []
+        self.voters = []
+
+        for i in range(5):
+            u = User.objects.create(username="test {:d}".format(i))
+            self.users.append(u)
+            c = Candidate.objects.create(election=self.election)
+            self.candidates.append(c)
+            v = Voter.objects.create(election=self.election, user=u)
+            self.voters.append(v)
+
+
+    def test_delete_election(self):
+        """When an election is deleted, its users (only if they don't have elections anymore) and its candidates are also deleted"""
+
+        e_pk = self.election.pk
+        c_pks = [candidate.pk for candidate in self.candidates]
+        v_pks = [voter.pk for voter in self.voters]
+        u_pks = [user.pk for user in self.users]
+
+
+        response = self.client.post('/election/manage/delete_election/%d/'%e_pk,
+                                    {'pk': e_pk})
+
+        try:
+            Election.objects.get(pk=e_pk)
+            raise FieldError("The election was not deleted")
+        except Election.DoesNotExist:
+            pass
+
+        for pk in c_pks:
+            try:
+                Candidate.objects.get(pk=pk)
+                raise FieldError("The candidate was not deleted")
+            except Candidate.DoesNotExist:
+                pass
+
+        for pk in u_pks:
+            try:
+                User.objects.get(pk=pk)
+                raise FieldError("The user was not deleted")
+            except User.DoesNotExist:
+                pass
+
+        for pk in v_pks:
+            try:
+                Voter.objects.get(pk=pk)
+                raise FieldError("The voter was not deleted")
+            except Voter.DoesNotExist:
+                pass
